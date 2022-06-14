57be7e5fa87a052731fb2bc50f8ab2544e9956ce
Abdullah <54276938+Abdul535@users.noreply.github.com>
2022-06-14 19:16:16 +0530

    Update Cell.js

--- src/Cell.js
+++ src/Cell.js
@@ -2,19 +2,6 @@ import React, {Component} from 'react'
 import "./Cell.css"
 
 
-/** A single cell on the board.
- *
- * This has no state --- just two props:
- *
- * - flipCellsAroundMe: a function rec'd from the board which flips this
- *      cell and the cells around of it
- *
- * - isLit: boolean, is this cell lit?
- *
- * This handles clicks --- by calling flipCellsAroundMe
- *
- **/
-
 class Cell extends Component {
   constructor(props) {
     super(props);
@@ -22,7 +9,6 @@ class Cell extends Component {
   }
 
   handleClick(evt) {
-    // call up to the board to flip cells around this cell
     this.props.flipCellsAroundMe();
   }
 
